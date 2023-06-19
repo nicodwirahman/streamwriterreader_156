76c663b6254c5bd124a4affcb7a96938ded50524
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-19 14:53:06 +0700

    1

--- streamwriterreader_156/iofile.cpp
+++ streamwriterreader_156/iofile.cpp
@@ -4,5 +4,20 @@
 using namespace std;
 
 int main() {
+	string baris;
 
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunujuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= menulis file, \'q'\ untuk keluar" << endl;
+
+//unlimited loop untuk menulis
+	while (true){
+		cout << "-";
+	//mendapatkan setiap karakter dalam satu baris
+	getline(cin, baris);
+	//loop akan berhenti jika andamemasukan karakter q;
+	if (baris == "q") break;
 }
