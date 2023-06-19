2f81bfd355c17bd7502bd7d1878580bba5869b52
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-19 15:08:21 +0700

    membuka file

--- iofileWithpath/iofileWithpath.cpp
+++ iofileWithpath/iofileWithpath.cpp
@@ -6,4 +6,11 @@ using namespace std;
 int main() {
 	string baris;
 	string namaFile;
+
+	cout << "masukan nama file : ";
+	cin >> namaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
 }
